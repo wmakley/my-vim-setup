c5bcf881423ffb34752acdb31afeceba9a396e29
William Makley <wmakley@tortus.com>
2013-11-25 13:53:42 -0500

    update command-t setup to ensure use of system ruby and not use rake

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 set -eu
 cd $(dirname "$0")
 git submodule init && git submodule update
-cd bundle/command-t
-rake make
+cd bundle/command-t/ruby/command-t
+/usr/bin/ruby extconf.rb
+make
 pushd $HOME
 ln -sfv .vim/vimrc .vimrc 
 ln -sfv .vim/gvimrc .gvimrc
