087911bda1e7270e0f0902c80e350fe067226dc7
William Makley <wmakley@tortus.com>
2013-12-16 10:27:42 -0500

    remove defunct submodules if they exist

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 set -eu
 cd "$(dirname "$0")"
 git submodule update --init
+# remove defunct submodules
+[ -d bundle/command-t ] && rm -rfv bundle/command-t
+[ -d bundle/snipmate-snippets ] && rm -rfv bundle/snipmate-snippets
 pushd $HOME
 ln -nsfv .vim/vimrc .vimrc
 ln -nsfv .vim/gvimrc .gvimrc
