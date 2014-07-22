8f32e2d5b70bdeb9f00d5a9ebbe0108e6e23116a
William Makley <wmakley@tortus.com>
2014-07-22 10:05:09 -0400

    fix setup to work with vundle

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 set -eu
 cd "$(dirname "$0")"
 
+rm -rfv bundle/*
+
 # check out plugins
 git submodule update --init
-
-# remove defunct plugins
-[ -d bundle/command-t ] && rm -rfv bundle/command-t
-[ -d bundle/snipmate-snippets ] && rm -rfv bundle/snipmate-snippets
 
 # sym link rc files
 ln -nsfv .vim/vimrc $HOME/.vimrc
 ln -nsfv .vim/gvimrc $HOME/.gvimrc
+
+vim +PluginInstall +qall
